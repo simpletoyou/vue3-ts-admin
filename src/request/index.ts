1fd39abc2c43cf50b2b876c1ba7fe08ad6815d37
simpletoyou <3361694095@qq.com>
2023-03-29 16:25:11 +0800

    axios封装

---
+++ src/request/index.ts
@@ -0,0 +1,41 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: ChenShuShu
+ * @Date: 2023-03-29 16:12:20
+ * @LastEditors: ChenShuShu
+ * @LastEditTime: 2023-03-29 16:24:28
+ */
+import axios from "axios";
+// 创建axios实例
+const service = axios.create({
+  baseURL: "https://www.fastmock.site/mock/bf1fcb3ce2945669c2c8d0ecb8009b8/api",
+  timeout: 5000,
+  headers: {
+    "Content-Type":"application/json;charset=utf-8"
+  }
+})
+
+//请求拦截
+service.interceptors.request.use((config)=>{
+  console.log({config})
+  config.headers = config.headers||{}
+  if(localStorage.getItem('token')) {
+    config.headers.token = localStorage.getItem('token')
+  }
+  return config
+})
+
+//响应拦截
+service.interceptors.response.use((res)=>{
+  console.log({res})
+  const code:number = res.data.code
+  if(code!=200) {
+    return Promise.reject(res.data)
+  }
+  return res.data
+},(err)=>{
+  console.log(err)
+})
+
+export default service;
