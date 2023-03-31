09a4202fd8b45b95b06d9ef0be31f14348a3d501
chenchuhua <3361694095@qq.com>
2023-03-31 14:09:00 +0800

    首页框架搭建

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,22 @@
  * @Author: ChenShuShu
  * @Date: 2023-03-29 13:03:27
  * @LastEditors: ChenShuShu
- * @LastEditTime: 2023-03-29 13:44:07
+ * @LastEditTime: 2023-03-31 13:40:48
 -->
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-
 * {
   padding: 0;
   margin: 0;
 }
-html, body, #app {
-width: 100%;
-height: 100%;
+
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
 }
 </style>
