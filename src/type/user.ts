61a591e2396d46cc3c25a67fccce1366750b4c6d
simpletoyou <3361694095@qq.com>
2023-04-16 17:59:42 +0800

    添加编辑弹窗

--- src/type/user.ts
+++ src/type/user.ts
@@ -1,35 +1,50 @@
 /*
- * @Description: 
- * @version: 
+ * @Description:
+ * @version:
  * @Author: ChenShuShu
  * @Date: 2023-03-31 22:22:08
  * @LastEditors: ChenShuShu
  * @LastEditTime: 2023-04-01 14:59:54
  */
 export interface ListInt {
-  id: number,
-  nickName: string,
-  userName: string,
-  role: RoleInt[]
+  id: number;
+  nickName: string;
+  userName: string;
+  role: RoleInt[];
 }
 interface RoleInt {
-  role: number,
-  roleName: string
+  role: number;
+  roleName: string;
 }
 interface SelectDataInt {
-  role: number,
-  nickName: string
+  role: number;
+  nickName: string;
 }
 interface RoleListInt {
-  authority: number[],
-  roleId: number,
-  roleName: string
+  authority: number[];
+  roleId: number;
+  roleName: string;
 }
+
+interface ActiveInt {
+  id: number;
+  nickName: string;
+  userName: string;
+  role: number[];
+}
+
 export class InitData {
-  list: ListInt[] = [] //存放接口返回的用户列表数据
+  list: ListInt[] = []; //存放接口返回的用户列表数据
   selectData: SelectDataInt = {
     role: 0,
-    nickName: ""
-  }
-  roleList: RoleListInt[] = []//存放接口返回的角色列表数据
+    nickName: "",
+  };
+  roleList: RoleListInt[] = []; //存放接口返回的角色列表数据
+  isShow = false;
+  active: ActiveInt = {
+    id: 0,
+    nickName: "",
+    userName: "",
+    role: [],
+  };
 }
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -26,12 +26,42 @@
             <span v-for="item in scope.row.role" :key="item.role">{{ item.roleName }}&nbsp;</span>
           </template>
         </el-table-column>
+        <!-- 操作 -->
+        <el-table-column prop="role" label="操作">
+          <template #default="scope">
+            <el-button type="primary" @click="changeUser(scope.row)">编辑</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </div>
+    <!-- 编辑弹窗 -->
+    <el-dialog v-model="isShow" title="Shipping address">
+      <el-form :model="active">
+        <el-form-item label="姓名" label-width="50px">
+          <el-input v-model="active.nickName" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="角色" label-width="50px">
+          <el-select multiple v-model="active.role" placeholder="请选择角色">
+            <el-option v-for="item in roleList" :key="item.roleId" :label="item.roleName" :value="item.roleId" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="isShow = false">Cancel</el-button>
+          <el-button type="primary" @click="confirmChange()">
+            Confirm
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
+
+
   </div>
 </template>
 
 <script lang="ts">
+import { it } from 'element-plus/es/locale';
 import { List } from 'lodash';
 import { defineComponent, onMounted, reactive, toRefs, watch } from 'vue'
 import { getUserList, getRoleList } from '../request/api';
@@ -76,13 +106,40 @@ export default defineComponent({
       }
       data.list = arr
     }
+
+    const changeUser = (item: ListInt) => {
+      console.log({ item })
+      data.active = {
+        id: item.id,
+        nickName: item.nickName,
+        userName: item.userName,
+        role: item.role.map((v: any) => v.role || v.roleId)
+      }
+      data.isShow = true
+    }
+
+    const confirmChange = () => {
+      console.log(data.active)
+      let obj: any = data.list.find((v) => { return v.id === data.active.id })
+
+      obj.nickName = data.active.nickName
+      obj.role = data.roleList.filter((v) => data.active.role.indexOf(v.roleId) != -1)
+      data.list.forEach(item => {
+        if (item.id === obj.id) {
+          item = obj
+        }
+      })
+      data.isShow = false
+    }
+
+
     // 监听输入框的两个属性
     watch([() => data.selectData.nickName, () => data.selectData.role], () => {
       if (data.selectData.nickName == '' || data.selectData.role == 0) {
         getUser()
       }
     })
-    return { ...toRefs(data), onSubmit }
+    return { ...toRefs(data), onSubmit, changeUser, confirmChange }
   }
 })
 </script>
